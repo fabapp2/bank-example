e935163a1396e295b6f394c9dea1df25b9710cff
fabapp <fkrueger@pivotal.io>
2020-02-13 15:01:01 +0100

    added missing media types using MediaType

--- banking-transaction/src/main/java/com/bank/transaction/AccountServiceFacade.java
+++ banking-transaction/src/main/java/com/bank/transaction/AccountServiceFacade.java
@@ -1,12 +1,13 @@
 package com.bank.transaction;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "accounts", url = "${accounts.feign.url}")
 public interface AccountServiceFacade {
-    @RequestMapping(method = RequestMethod.POST, value="/accounts/{accountNumber}/transactions", consumes = "application/json")
+    @RequestMapping(method = RequestMethod.POST, value="/accounts/{accountNumber}/transactions", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     BankAccount transactAccount(@PathVariable("accountNumber") String accountNumber, AccountTransactionVo accountTransactionVo);
 }
